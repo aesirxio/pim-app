dff570acb89cdcdd229f949255d6b1d19db37772
Viet Nguyen <viet.nguyen@redweb.dk>
2022-12-05 11:50:21 +0700

    Variant Price

---
+++ src/containers/ProductsPage/Component/Variants/ModalVariants.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Button, Col, Form, Nav, Row, Tab } from 'react-bootstrap';
+import { withTranslation } from 'react-i18next';
+import ModalComponent from 'components/Modal';
+import Label from 'components/Form/Label';
+import Input from 'components/Form/Input';
+const ModalVariants = ({
+  t,
+  dataTable,
+  optionVariants,
+  activeVariant,
+  showModal,
+  setShowModal,
+}) => {
+  const handleClose = () => {
+    setShowModal(false);
+  };
+  return (
+    <ModalComponent
+      show={showModal}
+      centered
+      onHide={handleClose}
+      header={<div className="fs-2 fw-bold mb-0">{t('txt_edit_price_variant')}</div>}
+      dialogClassName={'modal-xl'}
+      body={
+        <div className="">
+          <Tab.Container
+            id="left-tabs-fields"
+            defaultActiveKey={activeVariant ? 'variant-' + activeVariant.index : 'variant-0'}
+          >
+            <Row className="gx-24">
+              <Col lg={3}>
+                <div className="fs-14 pb-16 mb-1 border-bottom fw-semibold text-uppercase">
+                  {t('txt_variants')}
+                </div>
+                <Nav variant="tabs" className="flex-column">
+                  {dataTable?.map((item, key) => {
+                    const variantString = optionVariants.reduce(
+                      (prev, curr) => `${prev}${prev && '-'} ${item[curr.name.toLowerCase()]} `,
+                      ''
+                    );
+                    return (
+                      <Nav.Item key={key}>
+                        <Nav.Link eventKey={`variant-${key}`}>{variantString}</Nav.Link>
+                      </Nav.Item>
+                    );
+                  })}
+                </Nav>
+              </Col>
+              <Col lg={9}>
+                <Tab.Content>
+                  {dataTable?.map((item, key) => {
+                    return (
+                      <Tab.Pane key={key} eventKey={`variant-${key}`}>
+                        <Row className="gx-24">
+                          <Col lg={6}>
+                            <Form.Group className={`w-100`}>
+                              <Label text="Input Costs" />
+                              <Input
+                                field={{
+                                  value: item.price,
+                                  classNameInput: 'fs-14',
+                                  placeholder: t('txt_type'),
+                                  format: 'VND',
+                                  changed: (event) => {
+                                    // this.formPropsData.variants = event.target.value;
+                                    item.price = event.target.value;
+                                    console.log('event.target.value', event.target.value);
+                                    console.log('activeVariant', activeVariant);
+                                  },
+                                }}
+                              />
+                            </Form.Group>
+                          </Col>
+                        </Row>
+                      </Tab.Pane>
+                    );
+                  })}
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </div>
+      }
+      footer={
+        <>
+          <Button
+            variant={`success`}
+            className={`px-4 py-1 fw-bold mb-0 fs-14 lh-sm`}
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            {t('txt_submit')}
+          </Button>
+        </>
+      }
+    />
+  );
+};
+export default withTranslation('common')(ModalVariants);
