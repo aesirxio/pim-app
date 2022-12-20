c056c6e3ee0a9b748352e035ef29fa10d0d565c6
Viet Nguyen <viet.nguyen@redweb.dk>
2022-12-20 12:00:57 +0700

    Fix DAM package

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import ErrorBoundary from './layouts/ErrorBoundary';
 import i18n from 'translations/i18n';
 import { I18nextProvider } from 'react-i18next';
 import { ThemesContext, ThemesContextProvider } from 'themes/ThemeContextProvider';
-import 'aesirx-dam-app/dist/index.css';
-import 'aesirx-dam-app/dist/app.css';
+import '@kakahuy113/test-dam-app/dist/index.css';
+import '@kakahuy113/test-dam-app/dist/app.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
