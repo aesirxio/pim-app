5d5dea0e102a4b4251b0451019f24e256fd66852
Lu Nguyen <lu@r-digital.tech>
2023-02-18 13:32:41 +0700

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -12,5 +12,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsa
 reportWebVitals();
